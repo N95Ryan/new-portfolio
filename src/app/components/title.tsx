e627410d394f42fca6b4b25c8ef34d4078073075
Ryan <ryanpinas77@gmail.com>
2024-06-12 22:26:00 +0200

    title added

---
+++ src/app/components/title.tsx
@@ -0,0 +1,8 @@
+export function Title() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen space-y-4">
+            <h1 className="text-7xl font-extrabold bg-gradient-to-r from-violet-400 to-indigo-600 bg-clip-text text-transparent animate-fade-down animate-once animate-duration-[600ms] animate-delay-150 animate-ease-linear">Ryan PINA-SILASSE</h1>
+            <h2 className="text-2xl text-white animate-fade-up animate-once animate-duration-[600ms] animate-delay-150 animate-ease-linear" >Développeur Web Front-end</h2>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-export default function Page() {
-  return(
-  <div>
-    <h1>Ryan PINA-SILASSE</h1>
+import { Title } from "./components/title";
+import Particles from "./components/magicui/particles";
 
-  </div>
-  ) 
+export default function Page() {
+  return (
+    <div className="relative h-screen w-screen">
+      <Particles className="absolute top-0 left-0 h-full w-full z-0" />
+      <div className="relative z-10">
+        <Title />
+      </div>
+    </div>
+  );
 }
