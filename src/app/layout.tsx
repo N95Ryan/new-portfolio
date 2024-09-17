6a19b41b8d281f0b964ff4757f16c82393beb67e
Ryan <ryanpinas77@gmail.com>
2024-09-18 00:40:04 +0200

    update resume + SEO update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,12 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ryan PINA-SILASSE | Développeur Web Front-End React",
-  description: "Portfolio de Ryan PINA-SILASSE, développeur web front-end spécialisé en React. Découvrez mes projets, mes compétences et mon parcours.",
-  keywords: "Ryan PINA-SILASSE, Développeur Web Front-End, Île-de-France, Canada, React, Next.js, Portfolio, Hetic, Konbini, Développement Web, UI/UX Design, JavaScript, HTML5, CSS3",
+  title: "Ryan PINA-SILASSE | Développeur Web Front-End",
+  description:
+    "Portfolio de Ryan PINA-SILASSE, développeur web front-end spécialisé en React. Découvrez mes projets, mes compétences et mon parcours.",
+  keywords:
+    "Ryan PINA-SILASSE, Développeur Web Front-End, Front end developer, Paris, Île-de-France, Canada, Bootstrap, Tailwind, React Native, React, Next.js, Portfolio, Hetic, Konbini, Développement Web, UI/UX, Design, TypeScript, JavaScript, CSS3, HTML5",
   authors: [{ name: "Ryan PINA-SILASSE" }],
-  
 };
 
 export default function RootLayout({
